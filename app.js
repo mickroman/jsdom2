5bf819b1b2dde99d3cd2f3668f6f11d17229582a
mick roman <mickroman65@gmail.com>
2018-12-14 01:03:41 +0000

    test.txt

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const wrap = document.querySelector('#book-list li:nth-child(2) .name');
-console.log(wrap);
+const books = document.querySelectorAll('#book-list li .name');
 
-const wrap2 = document.getElementById("wrapper")
+Array.from(books).forEach(function(book){
+  book.textContent += ' (Book title)';
+});
 
+const bookList = document.querySelector('#book-list');
+bookList.innerHTML = '<h2>Books and more books...</h2>';
+bookList.innerHTML += '<p>This is how you add HTML content</p>';
